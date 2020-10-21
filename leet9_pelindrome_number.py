a34ddcc936a37ecc740ce9d9217519d586aac290
lk90 <lokesh.thakur90@gmail.com>
2020-10-20 19:13:44 -0500

    Add files via upload

---
+++ leet9_pelindrome_number.py
@@ -0,0 +1,42 @@
+"""
+Determine whether an integer is a palindrome. An integer is a palindrome
+when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it
+becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a 
+palindrome.
+Follow up:
+
+Could you solve it without converting the integer to a string?
+"""
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        temp = x
+        rev = 0
+        while x:
+            pop = x % 10
+            rev = rev * 10 + pop
+            x = int(x / 10)
+        return temp == rev
+
+
+# below code is for Test purpose
+y = Solution()
+print(y.isPalindrome(123))
